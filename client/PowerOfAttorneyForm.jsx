c0a8f65c5159dc3c4e9392d34d958ce2c838eba0
Abigail Watson <abigailwatson@Abigails-Mac-mini-317772.local>
2018-07-03 09:50:25 -0500

    clinical:alert

--- client/PowerOfAttorneyForm.jsx
+++ client/PowerOfAttorneyForm.jsx
@@ -3,7 +3,7 @@ import { insertContract, removeContractById, updateContract } from 'meteor/clini
 
 import { get, has, set } from 'lodash';
 
-import { Bert } from 'meteor/themeteorchef:bert';
+import { Bert } from 'meteor/clinical:alert';
 import React from 'react';
 import { ReactMeteorData } from 'meteor/react-meteor-data';
 import ReactMixin from 'react-mixin';
--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-contract',
-  version: '3.3.8',
+  version: '3.3.9',
   summary: 'HL7 FHIR Resource - Contract',
   git: 'https://github.com/clinical-meteor/hl7-resource-contract',
   documentation: 'README.md'
@@ -27,6 +27,7 @@ Package.onUse(function (api) {
   api.use('clinical:hl7-resource-datatypes@3.0.1');
   api.use('clinical:hl7-resource-bundle@1.3.10');
   api.use('matb33:collection-hooks@0.7.15');
+  api.use('clinical:alert@2.1.3');
 
   api.use('digilord:faker@1.0.7');
 
