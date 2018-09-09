d657c5755fcb7f76b989937f605863bd2df73cc8
Abigail Watson <abigailwatson@Abigails-Mac-mini-379644.local>
2018-09-09 01:48:33 -0500

    QA pass

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-contract',
-  version: '3.4.0',
+  version: '3.4.1',
   summary: 'HL7 FHIR Resource - Contract',
   git: 'https://github.com/clinical-meteor/hl7-resource-contract',
   documentation: 'README.md'
